76d8536198c08b2fafff83b8c8a81a1f69da3c89
jarvisbruder <jarvisbruder@gmail.com>
2023-03-26 23:53:39 -0400

    m

--- netlify/edge-functions/wapp.js
+++ netlify/edge-functions/wapp.js
@@ -5,23 +5,20 @@ export default async (request, context) => {
     const verify_token = Deno.env.get("VERIFY_TOKEN");
 
   
-    if (context.request && context.request.url.searchParams.get("hub.mode") === "subscribe" &&
-    context.request.url.searchParams.get("hub.verify_token") === verify_token) {
-
-      return new Response(JSON.stringify(
-        {status: 200,
-         body: context.request.url.searchParams.get("hub.challenge"),
-        })
-    );
+    const mode = context.request.url.searchParams.get("hub.mode");
+    const challenge = context.request.url.searchParams.get("hub.challenge");
   
-    } else {
+    if (mode && token) {
+      if (mode === "subscribe" && token === verify_token) {
+        context.response.status = 200;
+        context.response.body = challenge;
+      } else {
+        context.response.status = 403;
+      }
+    }
+
   
         return new Response(JSON.stringify({status: 404, body:"unmatched"}));
 
     }
-  
-  
-  
-  
-  }
-  
+   
